f439b7aa45c6cbbf06e71ae8fdb6ab5b9239dd1b
Thomas Kramer <TIKramer@users.noreply.github.com>
2022-08-27 23:35:53 +0800

    Replace tab screens with Search and About screens

---
+++ src/screens/RecipeScreen.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, ScrollView, } from 'react-native';
+import { List } from 'react-native-paper';
+import SimularRecipes from '../components/SimularRecipes';
+import Step from '../components/Step';
+import RecipeHeader from '../components/RecipeHeader';
+import { useSelector } from 'react-redux';
+
+export const RecipeScreen = () =>
+{
+    const { analyzedInstructions } = useSelector((state) => state.recipe );
+
+    return (
+
+        <View style={{ flex: 1 }}>
+
+            <ScrollView>
+                <RecipeHeader />
+                <List.Section title="Instructions">
+                    {analyzedInstructions[0]?.steps?.map((object: step) =>
+                    {
+                        return (
+                            <Step number={object.number} ingredients={object.ingredients} equipment={object.equipment} instructions={object.step}/> 
+                        )
+                    })}
+
+                </List.Section>
+                <SimularRecipes />
+            </ScrollView>
+
+        </View>
+
+    );
+};
+
+
