95ae14eb8006c25bf2bfbbc94c818feb8a7170dd
Thomas Kramer <TIKramer@users.noreply.github.com>
2022-08-27 17:59:24 +0800

    typeDefintions

---
+++ ts/nutrion.d.ts
@@ -0,0 +1,22 @@
+type nutritionalInformation
+ = {
+  calories: string,
+  carbs: string,
+  fat: string,
+  protein: string,
+  bad: nutrition[]
+  good: nutrition[]
+ }
+
+ type caloricBreakDown = {
+    percentCarbs: number,
+    percentFat: number,
+    percentProtein: number,
+  },
+ 
+type nutrition = {
+    title: string,
+  amount: string,
+  indented: boolean,
+  percentOfDailyNeeds: number
+}
---
+++ ts/recipes.d.ts
@@ -0,0 +1,75 @@
+type Recipe_Information =
+{
+  id: number,
+  title: string,
+  image: string,
+  imageType: string,
+  serving: number,
+  readyInMinutes: number,
+  license: string,
+  sourceName: string,
+  sourceUrl: string,
+  sponnacularSourceUrl: string,
+  aggregateLikes: number,
+  spoonacularScore: number,
+  pricePerServing: number
+  analyzedInstructions: instructions[],
+  
+
+
+}
+
+
+
+type Simular = {
+    id: number,
+    imageType: string,
+    readyInMinutes: number,
+    servings: number,
+    sourceUrl: string,
+    title: string,
+
+}
+
+type measurment = {
+    unit: string;
+    value: number;
+
+}
+type instructions = {
+    number: number,
+    steps: steps[],
+
+}
+
+
+type steps = {
+    number: number,
+    step: string,
+    ingredients: ingredient[],
+    equipment: equipment[],
+
+}
+
+type ingredient = {
+    amount?: {
+        metric: measurment
+        us: measurment
+    },
+    image: string,
+    name: string,
+    id?: number
+}
+
+
+type equipment = {
+    id: number,
+    image: string,
+    name: string,
+
+}
+type requiredItem = {
+    name: string,
+    image: string
+}
+
