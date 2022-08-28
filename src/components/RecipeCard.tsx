e10bb1fd3cc3b07f1441f26ba7fe4dcdf799fefe
Thomas Kramer <TIKramer@users.noreply.github.com>
2022-08-28 09:39:41 +0800

    Upload RecipeHeader and RecipeCard

---
+++ src/components/RecipeCard.tsx
@@ -0,0 +1,62 @@
+import { Card, Divider, Paragraph, Text } from "react-native-paper";
+import { bindActionCreators } from "redux";
+import { actionCreators } from '../../state';
+
+
+import { useSelector, useDispatch } from "react-redux";
+import { TouchableOpacity } from "react-native";
+import React from "react";
+import { useNavigation } from '@react-navigation/native';
+
+
+
+const RecipeCard = ({ id, title, image, missingCount }: { id: number, title: string, image: string, missingCount: number | undefined }) =>
+{
+    const navigation = useNavigation();
+
+    const dispatch = useDispatch();
+
+    const { setRecipeID,setMissingCount } =
+        bindActionCreators(actionCreators, dispatch);
+
+    const loadRecipe =() => {
+        setRecipeID(id)
+        navigation.navigate('Recipe')
+
+    }
+    return (
+
+        <Card style={styles.card}>
+            <TouchableOpacity onPress={() => loadRecipe()}>
+
+                <Card.Content>
+
+                </Card.Content>
+                <Card.Cover style={styles.cardImage} source={{ uri: image }} />
+                <Divider />
+                <Text style={styles.title} variant="bodyMedium">{title}</Text>
+                {missingCount ?
+                    <Paragraph>{missingCount > 0 ? `missing ${missingCount} ingredients` : 'you have all the ingredients'}</Paragraph>
+                    : null}
+                <Card.Actions>
+
+                </Card.Actions>
+            </TouchableOpacity>
+        </Card>
+
+    )
+
+};
+const styles = StyleSheet.create({
+    card: {
+        width: '45%', marginHorizontal: 5, marginVertical: 5
+    },
+    cardImage: {
+        height: 100 
+    }
+    title:{
+        fontWeight: 'bold' 
+    }
+  });
+  
+export default RecipeCard;
---
+++ src/components/RecipeHeader.tsx
@@ -0,0 +1,100 @@
+import { View, StyleSheet, Image, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import
+{
+  Subheading,
+  Title,
+} from "react-native-paper";
+import React from "react";
+import { useSelector } from "react-redux";
+
+const RecipeHeader = () =>
+{
+  const { missingCount, title, image, sourceName, readyInMinutes, aggregateLikes } = useSelector((state) => state.recipe);
+
+
+  return (
+    <View style={{
+      top: 0, alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+
+      <Image
+        style={styles.recipeImage}
+        source={{ uri: image }}
+      />
+      <View style={styles.recipeInfoContainer}>
+        <View style={{ marginHorizontal: 10 }}>
+          <View
+            style={
+              styles.recipeInfoContainerDivider
+            }
+          >
+            <Title>
+              {title}
+            </Title>
+
+            <Subheading>
+              {sourceName}
+            </Subheading>
+          </View>
+          {missingCount > 0 ?
+            <Text>You are missing {missingCount} ingredients</Text>
+            :
+            <Text>You have all the ingredients</Text>
+          }
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              {aggregateLikes > 0 ?
+                <><Text>{aggregateLikes}</Text><Ionicons name="heart" size={30} color="green" /></>
+                : null}
+
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+
+              <Ionicons name="time-outline" size={30} color="green" />
+              <Text>{readyInMinutes} mins</Text>
+            </View>
+
+
+          </View>
+        </View>
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+
+
+  recipeInfoContainerDivider:
+  {
+    borderBottomColor: 'black',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  recipeInfoContainer:
+  {
+    position: 'absolute',
+    bottom: 0,
+    backgroundColor: 'white',
+    width: '90%',
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  recipeImage: {
+    height: 240,
+    width: '100%',
+    resizeMode: 'contain',
+    marginBottom: 90
+  }
+
+});
+
+export default RecipeHeader;
