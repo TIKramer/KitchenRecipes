62376673df43ad33abc971e15cf6f1128b4cdf74
Thomas Kramer <TIKramer@users.noreply.github.com>
2022-08-28 18:12:18 +0800

    Update theme to only use React paper

--- App.tsx
+++ App.tsx
@@ -2,36 +2,42 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {  DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
 import { setAxiosCommonHeader } from './src/api';
-
+import { store } from './state/store';
 import useCachedResources from './src/hooks/useCachedResources';
 import useColorScheme from './src/hooks/useColorScheme';
 import Navigation from './src/navigation';
+
 
 
 const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: 'tomato',
-    secondary: 'yellow',
+    primary: "green",
+    accent: "blue",
   },
 };
+
+export type ThemeOverride = typeof theme
 export default function App() {
   const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
+      <Provider store={store}>
+
       <SafeAreaProvider>
         <PaperProvider theme={theme}>
 
-        <Navigation colorScheme={colorScheme} />
+        <Navigation  />
         <StatusBar />
         </PaperProvider>
       </SafeAreaProvider>
+      </Provider>
     );
   }
 }
